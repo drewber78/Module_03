a0c209c3501a3e17d71ff6f9f578a4394b89ca8d
drewber78 <drew.j.cochran@gmail.com>
2023-01-24 21:04:19 -0800

    Initial Git creation and push

---
+++ Assignment/Assignment 03.py
@@ -0,0 +1,8 @@
+"""
+Name: Drew Cochran
+Date: 24JAN2023
+Description: Script file to create and store a list of household items and those items value in a text file that can be
+read by the user. Also, additional items can be stored in the text file once the program is run again.
+Changelog:
+"""
+
