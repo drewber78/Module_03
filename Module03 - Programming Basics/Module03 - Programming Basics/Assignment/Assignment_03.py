f6b4a63877a4dc0fc4f78448a9f23514625a9c78
Drew Cochran <drew.j.cochran@outlook.com>
2023-01-30 13:43:59 -0800

    Final version

--- Module03 - Programming Basics/Module03 - Programming Basics/Assignment/Assignment_03.py
+++ Module03 - Programming Basics/Module03 - Programming Basics/Assignment/Assignment_03.py
@@ -5,6 +5,10 @@ Description: Script file to create and store a list of household items and those
 read by the user. Also, additional items can be stored in the text file once the program is run again.
 Changelog:
     26JAN2023: Writing new functions to call for opening document and apending the document.
+    27JAN2023: Corrected "None" error; program was displaying word "None" due to using input() with embedded print()
+    function. Oops.
+    30JAN2023: Opened to take screenshots for assignment learning document. No changes.
+
 """
 
 # Function to start and open the text file
@@ -21,7 +25,7 @@ def HouseHoldItemTxtFile():
     while(True):
 
         # Items is the primary variable, determines if user is entering new household item or wants to end the program
-        Items = input(print("Enter an Item (Or type 'Exit' to quit): "))
+        Items = input("Enter an Item (Or type 'Exit' to quit): ")
 
         # If statement to determine if user wants to end program
         if(Items.lower() == "exit"):
@@ -30,7 +34,7 @@ def HouseHoldItemTxtFile():
         # Else statement to write household items to the file, followed by value of items
         else:
             HouseHoldItems.write(Items + ', ')
-            Value = input(print("Enter an estimated Value: "))
+            Value = input("Enter an estimated Value: ")
             HouseHoldItems.write(Value + '\n')
 
     HouseHoldItems.close()
